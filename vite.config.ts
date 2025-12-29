33d29816061fb4eebfd6499cb98b619a0847e6a9
yexss <ye.xss.ran@gmail.com>
2025-12-29 22:23:57 +0800

    1.添加配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      server: {
+        base: '/chat-history-insight-pro/',
+        server: {
         port: 3000,
         host: '0.0.0.0',
       },
